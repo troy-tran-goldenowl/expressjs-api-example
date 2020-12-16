73c18ad7a7a422091a9b9159508f15edad0b5f4e
Troy Tran <troy.tran.goldenowl@gmail.com>
2020-12-16 12:25:36 +0700

    adding authorization check

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const postRoutes = require('./post.route');
 const authRoutes = require('./auth.route');
+const auth = require('../../middlewares/auth');
 
 const router = express.Router();
 
-router.use('/posts', postRoutes);
+router.use('/posts', auth, postRoutes);
 router.use('/auth', authRoutes);
 
 module.exports = router;
